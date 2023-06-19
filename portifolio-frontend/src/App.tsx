5d79bbaf64cf6b705154c4ae8678bfdb1816deae
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2023-06-19 11:49:32 -0300

    feat: instalação styled-components

--- portifolio-frontend/src/App.tsx
+++ portifolio-frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
+import { HeaderComponent } from "./components/HeaderComponent";
 
 function App() {
-
   return (
     <>
-      <div className='background'></div>
-      
+      <HeaderComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ portifolio-frontend/src/components/HeaderComponent/index.tsx
@@ -0,0 +1,13 @@
+
+
+export const HeaderComponent = () => {
+  return (
+    <div className='CorpoHeader'>
+        <p>INICIO</p>
+        <p>SOBRE</p>
+        <p>STACKS</p>
+        <p>PROJETOS</p>
+        <p>CONTATO</p>
+    </div>
+  )
+}
